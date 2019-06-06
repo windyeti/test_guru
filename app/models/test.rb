c16db8092066942de8c5e6aa8e0d1d48b260b1ab
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2019-06-06 13:22:29 +0300

    delete belongs_to from questio.rb, answer.rb

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
   def self.test_in_category(category)
     category = Category.find_by_title(category)
-    Test.where('category_id == ?', category.id)
+    Test.where('category_id == ?', category.id).order(:desc).pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :resolved_tests
+  has_many :tests, through: resolved_tests
+
   def list_test_user(level)
 
   end
