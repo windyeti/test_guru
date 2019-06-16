866a6398af47c34252b81b117bdf30d265416c6b
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2019-06-16 21:06:57 +0300

    add controller Questions

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,51 @@
+class QuestionsController < ApplicationController
+  before_action :find_test, except: [:show, :destroy, :update, :edit]
+  before_action :find_question, only: [:show, :destroy]
+
+  helper_method :current_test_id
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
+  def index
+    list = @test.questions.map(&:body).join(", ")
+    render plain: list
+  end
+
+  def show
+    render plain: @question.body
+  end
+
+  def new
+  end
+
+  def create
+    question = Question.create(question_params)
+    render plain: question.body
+  end
+
+  def destroy
+    @question.destroy
+    render plain: "Question was destroyed"
+  end
+
+  def current_test_id
+    @test.id
+  end
+
+  private
+
+  def rescue_with_question_not_found
+    render plain: "Question not found!!!"
+  end
+  def question_params
+    params.require(:question).permit(:body, :test_id)
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  # get 'questions/index'
+  # get 'questions/show'
+  # get 'questions/create'
+  # get 'questions/destroy'
+  # get 'tests/index'
+  # get 'tests/show'
+  # get 'tests/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :tests do
+      resources :questions, shallow: true
+  end
+
 end
