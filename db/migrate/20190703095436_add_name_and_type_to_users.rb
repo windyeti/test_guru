d1aa8422c6570a731cb39078940d8939bddaea4c
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2019-07-03 16:15:16 +0300

    add model Admin

---
+++ db/migrate/20190703095436_add_name_and_type_to_users.rb
@@ -0,0 +1,7 @@
+class AddNameAndTypeToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :type, :string, null: false, default: 'User'
+  end
+end
