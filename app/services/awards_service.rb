93951a7c2b7aa3538b75490a4b3f2d79b7aec251
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2019-08-10 21:45:53 +0300

    add can get more then 1 badge for every rule

--- app/services/awards_service.rb
+++ app/services/awards_service.rb
@@ -13,14 +13,26 @@ class AwardsService
     @user.test_passages.where(passed: true).map(&:test).map(&:id)
   end
 
+  def ids_test_successful_within(time_range)
+    @user.test_passages.where(passed: true, created_at: time_range).map(&:test).map(&:id)
+  end
+
+  def ids_successful(badge_rule_value, badge)
+    if badge_rule_value.exists?
+      time_create_last_badge_category = BadgeUser.where(user_id: @user.id, badge_id: badge.id).last.created_at
+      ids_test_successful_within(time_create_last_badge_category..Time.current)
+    else
+      ids_test_successful
+    end
+  end
+
   def all_tests_category(badge)
     return false unless @test_passage.passed
     return false unless badge.rule_value == @test.category.title
-    # badge_category = @user.badges.where(rule_value: @test.category.title)
-    # return false if badge_category.exists?
+    badge_category = @user.badges.where(rule_value: @test.category.title)
 
     ids_test_category = Test.all.ids_test_category(@test.category.title)
-    (ids_test_category - ids_test_successful).empty?
+    (ids_test_category - ids_successful(badge_category, badge)).empty?
   end
 
   def first_attempt_ok(_badge)
@@ -31,10 +43,9 @@ class AwardsService
   def all_tests_level(badge)
     return false unless @test_passage.passed
     return false unless badge.rule_value.to_i == @test.level
-    # badge_level = @user.badges.where(rule_value: @test.level)
-    # return false if badge_level.exists?
+    badge_level = @user.badges.where(rule_value: @test.level)
 
     ids_test_level = Test.where(level: @test.level).ids
-    (ids_test_level - ids_test_successful).empty?
+    (ids_test_level - ids_successful(badge_level, badge)).empty?
   end
 end
