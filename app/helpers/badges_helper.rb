ec787b1d9b60b6380eab852953b70988e48ebbc5
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2019-08-10 12:38:32 +0300

    move const RULES to model Badge

--- app/helpers/badges_helper.rb
+++ app/helpers/badges_helper.rb
@@ -1,26 +1,14 @@
 module BadgesHelper
-  RULE_TITLE = {
-    'all_tests_category'=> I18n.t('admin.badge.all_tests_category'),
-    'first_attempt_ok'=> I18n.t('admin.badge.first_attempt_ok'),
-    'all_tests_level'=> I18n.t('admin.badge.all_tests_level')
-  }
+    def rules_titles
+    Badge::RULES.map { |rule| [ I18n.t("admin.badge.#{rule}"), rule ] }
+  end
 
-  RULE_IMAGE = {
-    'gold'=> I18n.t('admin.badge.gold'),
-    'silver'=> I18n.t('admin.badge.silver'),
-    'bronze'=> I18n.t('admin.badge.bronze')
-  }
+  def rules_images
+    Badge::IMAGES.map { |image| [ I18n.t("admin.badge.#{image}"), image ] }
+  end
 
   def header(badge)
-    # замена case
-    # это работает на 50% -- при переключении языка нет правильного перевода
-    # "#{I18n.t('admin.badge.rule', text: RULE_TITLE[badge.rule] )} #{badge.rule_value}"
-
-    case badge.rule
-    when 'all_tests_category' then ("#{I18n.t('admin.badge.all_tests_category')} #{badge.rule_value}")
-    when 'first_attempt_ok' then ("#{I18n.t('admin.badge.first_attempt_ok')}")
-    when 'all_tests_level' then ("#{I18n.t('admin.badge.all_tests_level')} #{badge.rule_value}")
-    end
+    I18n.t("admin.badge.#{badge.rule}")+ ' ' +"#{badge.rule_value}"
   end
 
   def image(badge)
@@ -29,9 +17,5 @@ module BadgesHelper
 
   def list_rule_value
     Category.all.pluck(:title) + Test.all.pluck(:level).uniq
-  end
-
-  def rule_to_invert_array(rule)
-    rule.map { |key, value| [ value, key ] }
   end
 end
--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,14 +1,12 @@
 class Badge < ApplicationRecord
+  RULES = %w{all_tests_category first_attempt_ok all_tests_level}
+  IMAGES = %w{gold silver bronze}
+
   has_many :badge_users
   has_many :users, through: :badge_users
 
   validates :rule, :image, presence: true
   validates :rule, uniqueness: { scope: :rule_value}
-
-  validate :rule_correct, on: :create
-
-  def rule_correct
-    errors.add(:rule_correct, I18n.t('admin.badge.errors')) if BadgesHelper::RULE_TITLE.map { |key| key == rule }.empty?
-  end
-
+  validates :rule, inclusion: { in: RULES }
+  validates :image, inclusion: { in: IMAGES }
 end
