48ef5e7e46054a8278f19c63baa9966354a0e4ac
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2020-01-31 14:31:16 +0300

    detele change from config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,6 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "test_guru_#{Rails.env}"
 
-  config.action_mailer.perform_deliveries = true
 
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'tikhanin.herokuapp.com' }
