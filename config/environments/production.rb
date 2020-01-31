b8dac7422a722aca0be6df1def37f3b23df97538
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2020-01-31 14:24:34 +0300

    add config.action_mailer.perform_deliveries = true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,8 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "test_guru_#{Rails.env}"
 
+  config.action_mailer.perform_deliveries = true
+
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'tikhanin.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
